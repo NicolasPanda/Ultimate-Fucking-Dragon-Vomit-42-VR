42555905d2402e2de80913f5184d0ee62d0f5281
NicolasPanda <91343504+NicolasPanda@users.noreply.github.com>
2022-04-26 13:16:37 -0400

    shoot

--- Assets/Controller/Shooting.cs
+++ Assets/Controller/Shooting.cs
@@ -18,30 +18,33 @@ public class Shooting : MonoBehaviour
     private void Shoot()
     {
         Debug.Log("shoot");
-        RaycastHit hit;
         var t = transform;
         Debug.DrawRay(t.position, t.forward * 500f, Color.green);
         
+        
+        var hits  = Physics.RaycastAll(transform.position, transform.forward, 500.0F); 
+        
         //For creating line renderer object
         var lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
-        lineRenderer.startColor = Color.blue;
-        lineRenderer.endColor = Color.blue;
+        lineRenderer.startColor = Color.white;
+        lineRenderer.endColor = Color.white;
         lineRenderer.startWidth = 0.01f;
         lineRenderer.endWidth = 0.01f;
         lineRenderer.positionCount = 2;
+        Material whiteDiffuseMat = new Material(Shader.Find("Unlit/Texture"));
+        lineRenderer.material = whiteDiffuseMat;
         lineRenderer.useWorldSpace = false;    
                 
-//For drawing line in the world space, provide the x,y,z values
+        //For drawing line in the world space, provide the x,y,z values
         lineRenderer.SetPosition(0, transform.position); //x,y and z position of the starting point of the line
         lineRenderer.SetPosition(1, transform.forward * 500f); //x,y and z position of the end point of the line
         
-        if (Physics.Raycast(t.position, t.forward, out hit, 500f))
+        foreach (var hit in hits)
         {
             var entity = (IEntity) hit.collider.gameObject.GetComponent<IEntity>();
-            if (entity != null)
-            {
-                entity.DamageEntity();
-            }
+            if (entity == null) continue;
+
+            entity.DamageEntity();
         }
     }
 }
