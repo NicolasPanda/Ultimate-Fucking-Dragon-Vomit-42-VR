ef6022e33d78de7c217d193f36515d75dcd205ed
NicolasPanda <91343504+NicolasPanda@users.noreply.github.com>
2022-04-26 12:00:26 -0400

    shoot

--- Assets/Controller/Shooting.cs
+++ Assets/Controller/Shooting.cs
@@ -20,22 +20,22 @@ public class Shooting : MonoBehaviour
         Debug.Log("shoot");
         RaycastHit hit;
         var t = transform;
-        Debug.DrawRay(t.position, t.forward * 100f, Color.green);
+        Debug.DrawRay(t.position, t.forward * 500f, Color.green);
         
         //For creating line renderer object
         var lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
-        lineRenderer.startColor = Color.red;
-        lineRenderer.endColor = Color.red;
+        lineRenderer.startColor = Color.blue;
+        lineRenderer.endColor = Color.blue;
         lineRenderer.startWidth = 0.01f;
         lineRenderer.endWidth = 0.01f;
         lineRenderer.positionCount = 2;
-        lineRenderer.useWorldSpace = true;    
+        lineRenderer.useWorldSpace = false;    
                 
 //For drawing line in the world space, provide the x,y,z values
         lineRenderer.SetPosition(0, transform.position); //x,y and z position of the starting point of the line
-        lineRenderer.SetPosition(1, transform.forward * 100f); //x,y and z position of the end point of the line
+        lineRenderer.SetPosition(1, transform.forward * 500f); //x,y and z position of the end point of the line
         
-        if (Physics.Raycast(t.position, t.forward, out hit, 100f))
+        if (Physics.Raycast(t.position, t.forward, out hit, 500f))
         {
             var entity = (IEntity) hit.collider.gameObject.GetComponent<IEntity>();
             if (entity != null)
