08f0085cb98c970e8ce26bbd8a15311b2333e97b
NicolasPanda <91343504+NicolasPanda@users.noreply.github.com>
2022-04-27 10:12:25 -0400

    Scout

--- Assets/Controller/Scout.cs
+++ Assets/Controller/Scout.cs
@@ -1,14 +1,21 @@
+using System;
 using PathCreation;
 using UnityEngine;
 
-public class PathController : MonoBehaviour
+public class Scout : MonoBehaviour
 {
     [Header("Path setting")]
     [SerializeField] private PathCreator pathCreator;
     [SerializeField] private EndOfPathInstruction endOfPathInstruction;
     [SerializeField] private float speed = 5;
+    [SerializeField] private float offsetToPlayer = 5;
     
     private float _distanceTravelled;
+
+    private void Start()
+    {
+        _distanceTravelled += offsetToPlayer;
+    }
 
     // Update is called once per frame
     void Update()
